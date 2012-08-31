82e9cd16c730b8ea56880d1dce027c19727d3ba5
dwickern <dwickern@s4isystems.com>
2012-08-31 13:44:58 -0700

    Add batch script to generate eclipse project

---
+++ generate-eclipse-project.bat
@@ -0,0 +1,8 @@
+@echo off
+echo This will use Maven to generate an eclipse project
+echo This will generate a .project, .classpath and META-INF/MANIFEST.MF
+echo This must be used when dependencies are added or modified
+echo Do not manually edit the generated files
+echo More info: http://felix.apache.org/site/apache-felix-maven-bundle-plugin-bnd.html
+
+mvn clean package eclipse:eclipse -Declipse.pde install
