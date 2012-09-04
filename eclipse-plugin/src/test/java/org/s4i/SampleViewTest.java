8e1d6a16b3bcceae1ad810e9852057ed5fc268b3
dwickern <dwickern@s4isystems.com>
2012-09-04 09:03:39 -0700

    Add unit test

---
+++ eclipse-plugin/src/test/java/org/s4i/SampleViewTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.s4i;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dwickern
+ */
+public class SampleViewTest {
+    
+    public SampleViewTest() {
+    }
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
