273ee97ceb231ab671d2200ba4f2fb600b560b2a
dwickern <dwickern@s4isystems.com>
2012-09-05 10:01:03 -0700

    Add eclipse integration for dependencies

--- generate-eclipse-project.bat
+++ generate-eclipse-project.bat
@@ -1,6 +1,6 @@
 @echo off
 echo This will use Maven to generate an eclipse project
-echo This will generate a .project, .classpath and META-INF/MANIFEST.MF
+echo This will generate a .project, .classpath, META-INF/MANIFEST.MF, and lib/ with jar dependencies
 echo This must be used when dependencies are added or modified
 echo Do not manually edit the generated files
 echo More info: http://felix.apache.org/site/apache-felix-maven-bundle-plugin-bnd.html
